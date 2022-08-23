add8a8a6cf03caeb609c5e5bab836cbff07392e0
Zied Bouazizi <bouazizizied05@gmail.com>
2022-08-23 12:17:18 +0100

    Translate students information page to arabic

--- resources/views/pages/support_team/students/list.blade.php
+++ resources/views/pages/support_team/students/list.blade.php
@@ -1,18 +1,18 @@
 @extends('layouts.master')
-@section('page_title', 'Student Information - '.$my_class->name)
+@section('page_title', 'بيانات الطالب - '.$my_class->name)
 @section('content')
 
-<div class="card">
+<div class="card text-right">
     <div class="card-header header-elements-inline">
-        <h6 class="card-title">Students List</h6>
+        <h6 class="card-title">قائمة الطلاب</h6>
         {!! Qs::getPanelOptions() !!}
     </div>
 
     <div class="card-body">
         <ul class="nav nav-tabs nav-tabs-highlight">
-            <li class="nav-item"><a href="#all-students" class="nav-link active" data-toggle="tab">All {{ $my_class->name }} Students</a></li>
+            <li class="nav-item"><a href="#all-students" class="nav-link active" data-toggle="tab">جميع طلاب {{ $my_class->name }}</a></li>
             <li class="nav-item dropdown">
-                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">Sections</a>
+                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">الصفوف</a>
                 <div class="dropdown-menu dropdown-menu-right">
                     @foreach($sections as $s)
                     <a href="#s{{ $s->id }}" class="dropdown-item" data-toggle="tab">{{ $my_class->name.' '.$s->name }}</a>
@@ -26,13 +26,13 @@
                 <table class="table datatable-button-html5-columns">
                     <thead>
                         <tr>
-                            <th>S/N</th>
-                            <th>Photo</th>
-                            <th>Name</th>
-                            <th>ADM_No</th>
-                            <th>Section</th>
-                            <th>Email</th>
-                            <th>Action</th>
+                            <th>العدد</th>
+                            <th>الصورة</th>
+                            <th>الإسم</th>
+                            <th>رقم التسجيل</th>
+                            <th>الصف</th>
+                            <th>البريد الإلكتروني</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -52,16 +52,16 @@
                                         </a>
 
                                         <div class="dropdown-menu dropdown-menu-left">
-                                            <a href="{{ route('students.show', Qs::hash($s->id)) }}" class="dropdown-item"><i class="icon-eye"></i> View Profile</a>
+                                            <a href="{{ route('students.show', Qs::hash($s->id)) }}" class="dropdown-item"><i class="icon-eye"></i> عرض الحساب</a>
                                             @if(Qs::userIsTeamSA())
-                                            <a href="{{ route('students.edit', Qs::hash($s->id)) }}" class="dropdown-item"><i class="icon-pencil"></i> Edit</a>
-                                            <a href="{{ route('st.reset_pass', Qs::hash($s->user->id)) }}" class="dropdown-item"><i class="icon-lock"></i> Reset password</a>
+                                            <a href="{{ route('students.edit', Qs::hash($s->id)) }}" class="dropdown-item"><i class="icon-pencil"></i> تعديل</a>
+                                            <a href="{{ route('st.reset_pass', Qs::hash($s->user->id)) }}" class="dropdown-item"><i class="icon-lock"></i> إعادة تعيين كلمة السر</a>
                                             @endif
                                             <!-- <a target="_blank" href="{{ route('marks.year_selector', Qs::hash($s->user->id)) }}" class="dropdown-item"><i class="icon-check"></i> Marksheet</a> -->
 
                                             {{--Delete--}}
                                             @if(Qs::userIsSuperAdmin())
-                                            <a id="{{ Qs::hash($s->user->id) }}" onclick="confirmDelete(this.id)" href="#" class="dropdown-item"><i class="icon-trash"></i> Delete</a>
+                                            <a id="{{ Qs::hash($s->user->id) }}" onclick="confirmDelete(this.id)" href="#" class="dropdown-item"><i class="icon-trash"></i> حذف</a>
                                             <form method="post" id="item-delete-{{ Qs::hash($s->user->id) }}" action="{{ route('students.destroy', Qs::hash($s->user->id)) }}" class="hidden">@csrf @method('delete')</form>
                                             @endif
                                         </div>
@@ -79,12 +79,12 @@
                 <table class="table datatable-button-html5-columns">
                     <thead>
                         <tr>
-                            <th>S/N</th>
-                            <th>Photo</th>
-                            <th>Name</th>
-                            <th>ADM_No</th>
-                            <th>Email</th>
-                            <th>Action</th>
+                            <th>العدد</th>
+                            <th>الصورة</th>
+                            <th>الإسم</th>
+                            <th>رقم التسجيل</th>
+                            <th>البريد الإلكتروني</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -103,16 +103,16 @@
                                         </a>
 
                                         <div class="dropdown-menu dropdown-menu-right">
-                                            <a href="{{ route('students.show', Qs::hash($sr->id)) }}" class="dropdown-item"><i class="icon-eye"></i> View Info</a>
+                                            <a href="{{ route('students.show', Qs::hash($sr->id)) }}" class="dropdown-item"><i class="icon-eye"></i> عرض الحساب</a>
                                             @if(Qs::userIsTeamSA())
-                                            <a href="{{ route('students.edit', Qs::hash($sr->id)) }}" class="dropdown-item"><i class="icon-pencil"></i> Edit</a>
-                                            <a href="{{ route('st.reset_pass', Qs::hash($sr->user->id)) }}" class="dropdown-item"><i class="icon-lock"></i> Reset password</a>
+                                            <a href="{{ route('students.edit', Qs::hash($sr->id)) }}" class="dropdown-item"><i class="icon-pencil"></i> تعديل</a>
+                                            <a href="{{ route('st.reset_pass', Qs::hash($sr->user->id)) }}" class="dropdown-item"><i class="icon-lock"></i> إعادة تعيين كلمة السر</a>
                                             @endif
-                                            <a href="#" class="dropdown-item"><i class="icon-check"></i> Marksheet</a>
+                                            <!-- <a href="#" class="dropdown-item"><i class="icon-check"></i> Marksheet</a> -->
 
                                             {{--Delete--}}
                                             @if(Qs::userIsSuperAdmin())
-                                            <a id="{{ Qs::hash($sr->user->id) }}" onclick="confirmDelete(this.id)" href="#" class="dropdown-item"><i class="icon-trash"></i> Delete</a>
+                                            <a id="{{ Qs::hash($sr->user->id) }}" onclick="confirmDelete(this.id)" href="#" class="dropdown-item"><i class="icon-trash"></i> حذف</a>
                                             <form method="post" id="item-delete-{{ Qs::hash($sr->user->id) }}" action="{{ route('students.destroy', Qs::hash($sr->user->id)) }}" class="hidden">@csrf @method('delete')</form>
                                             @endif
 
