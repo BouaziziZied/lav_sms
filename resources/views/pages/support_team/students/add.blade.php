cf43279d3c7df8b75855e8a27c5de434911bcf99
Zied Bouazizi <bouazizizied05@gmail.com>
2022-08-23 12:01:13 +0100

    Translate add student page to arabic

--- resources/views/pages/support_team/students/add.blade.php
+++ resources/views/pages/support_team/students/add.blade.php
@@ -1,81 +1,81 @@
 @extends('layouts.master')
-@section('page_title', 'Admit Student')
+@section('page_title', 'إضافة طالب')
 @section('content')
-<div class="card">
+<div class="card text-right">
     <div class="card-header bg-white header-elements-inline">
-        <h6 class="card-title">Please fill The form Below To Admit A New Student</h6>
+        <h6 class="card-title">يرجى ملأ النموذج لإضافة طالب جديد</h6>
 
         {!! Qs::getPanelOptions() !!}
     </div>
 
     <form id="ajax-reg" method="post" enctype="multipart/form-data" class="wizard-form steps-validation" action="{{ route('students.store') }}" data-fouc>
         @csrf
-        <h6>Personal data</h6>
+        <h6>البيانات الشخصية</h6>
         <fieldset>
             <div class="row">
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label>Full Name: <span class="text-danger">*</span></label>
-                        <input value="{{ old('name') }}" required type="text" name="name" placeholder="Full Name" class="form-control">
+                        <label>الاسم الكامل: <span class="text-danger">*</span></label>
+                        <input value="{{ old('name') }}" required type="text" name="name" placeholder="الاسم الكامل" class="form-control">
                     </div>
                 </div>
 
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label>Address: <span class="text-danger">*</span></label>
-                        <input value="{{ old('address') }}" class="form-control" placeholder="Address" name="address" type="text" required>
+                        <label>العنوان: <span class="text-danger">*</span></label>
+                        <input value="{{ old('address') }}" class="form-control" placeholder="العنوان" name="address" type="text" required>
                     </div>
                 </div>
             </div>
 
             <div class="row">
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="form-group">
-                        <label>Email address: </label>
-                        <input type="email" value="{{ old('email') }}" name="email" class="form-control" placeholder="Email Address">
+                        <label>البريد الالكترونى: </label>
+                        <input type="email" value="{{ old('email') }}" name="email" class="form-control" placeholder="البريد الالكترونى">
                     </div>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="form-group">
-                        <label for="gender">Gender: <span class="text-danger">*</span></label>
-                        <select class="select form-control" id="gender" name="gender" required data-fouc data-placeholder="Choose..">
+                        <label for="gender">الجنس: <span class="text-danger">*</span></label>
+                        <select class="select form-control" id="gender" name="gender" required data-fouc data-placeholder="إختر...">
                             <option value=""></option>
-                            <option {{ (old('gender') == 'Male') ? 'selected' : '' }} value="Male">Male</option>
-                            <option {{ (old('gender') == 'Female') ? 'selected' : '' }} value="Female">Female</option>
+                            <option {{ (old('gender') == 'Male') ? 'selected' : '' }} value="Male">ذكر</option>
+                            <option {{ (old('gender') == 'Female') ? 'selected' : '' }} value="Female">أنثى</option>
                         </select>
                     </div>
                 </div>
 
-                <div class="col-md-3">
+                <div class="col-md-4">
                     <div class="form-group">
-                        <label>Phone:</label>
-                        <input value="{{ old('phone') }}" type="text" name="phone" class="form-control" placeholder="">
+                        <label>الهاتف:</label>
+                        <input value="{{ old('phone') }}" type="text" name="phone" class="form-control" placeholder="الهاتف">
                     </div>
                 </div>
 
-                <div class="col-md-3">
+                <!-- <div class="col-md-3">
                     <div class="form-group">
                         <label>Telephone:</label>
                         <input value="{{ old('phone2') }}" type="text" name="phone2" class="form-control" placeholder="">
                     </div>
-                </div>
+                </div> -->
 
             </div>
 
             <div class="row">
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label>Date of Birth:</label>
-                        <input name="dob" value="{{ old('dob') }}" type="text" class="form-control date-pick" placeholder="Select Date...">
+                        <label>تاريخ الولادة:</label>
+                        <input name="dob" value="{{ old('dob') }}" type="text" class="form-control date-pick" placeholder="إختر التاريخ...">
 
                     </div>
                 </div>
 
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="nal_id">Nationality: <span class="text-danger">*</span></label>
-                        <select data-placeholder="Choose..." required name="nal_id" id="nal_id" class="select-search form-control">
+                        <label for="nal_id">الجنسية: <span class="text-danger">*</span></label>
+                        <select data-placeholder="اختر..." required name="nal_id" id="nal_id" class="select-search form-control">
                             <option value=""></option>
                             @foreach($nationals as $nal)
                             <option {{ (old('nal_id') == $nal->id ? 'selected' : '') }} value="{{ $nal->id }}">{{ $nal->name }}</option>
@@ -86,9 +86,9 @@
 
                 <div class="col-md-6">
                     <div class="form-group">
-                        <label class="d-block">Upload Photo:</label>
+                        <label class="d-block">الصورة:</label>
                         <input value="{{ old('photo') }}" accept="image/*" type="file" name="photo" class="form-input-styled" data-fouc>
-                        <span class="form-text text-muted">Accepted Images: jpeg, png. Max file size 2Mb</span>
+                        <span class="form-text text-muted">الصور المقبولة: png ،jpeg، الحد الأقصى لحجم الصورة 2Mb.</span>
                     </div>
                 </div>
 
@@ -134,13 +134,13 @@
 
         </fieldset>
 
-        <h6>Student Data</h6>
+        <h6>بيانات الطالب</h6>
         <fieldset>
             <div class="row">
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="my_class_id">Class: <span class="text-danger">*</span></label>
-                        <select onchange="getClassSections(this.value)" data-placeholder="Choose..." required name="my_class_id" id="my_class_id" class="select-search form-control">
+                        <label for="my_class_id">القسم: <span class="text-danger">*</span></label>
+                        <select onchange="getClassSections(this.value)" data-placeholder="إختر..." required name="my_class_id" id="my_class_id" class="select-search form-control">
                             <option value=""></option>
                             @foreach($my_classes as $c)
                             <option {{ (old('my_class_id') == $c->id ? 'selected' : '') }} value="{{ $c->id }}">{{ $c->name }}</option>
@@ -151,8 +151,8 @@
 
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="section_id">Section: <span class="text-danger">*</span></label>
-                        <select data-placeholder="Select Class First" required name="section_id" id="section_id" class="select-search form-control">
+                        <label for="section_id">الفصول: <span class="text-danger">*</span></label>
+                        <select data-placeholder="إختر القسم أولا" required name="section_id" id="section_id" class="select-search form-control">
                             <option {{ (old('section_id')) ? 'selected' : '' }} value="{{ old('section_id') }}">{{ (old('section_id')) ? 'Selected' : '' }}</option>
                         </select>
                     </div>
@@ -160,8 +160,8 @@
 
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="my_parent_id">Parent: </label>
-                        <select data-placeholder="Choose..." name="my_parent_id" id="my_parent_id" class="select-search form-control">
+                        <label for="my_parent_id">الولي: </label>
+                        <select data-placeholder="إختر..." name="my_parent_id" id="my_parent_id" class="select-search form-control">
                             <option value=""></option>
                             @foreach($parents as $p)
                             <option {{ (old('my_parent_id') == Qs::hash($p->id)) ? 'selected' : '' }} value="{{ Qs::hash($p->id) }}">{{ $p->name }}</option>
@@ -172,8 +172,8 @@
 
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label for="year_admitted">Year Admitted: <span class="text-danger">*</span></label>
-                        <select data-placeholder="Choose..." required name="year_admitted" id="year_admitted" class="select-search form-control">
+                        <label for="year_admitted">سنة الإضافة: <span class="text-danger">*</span></label>
+                        <select data-placeholder="إختر..." required name="year_admitted" id="year_admitted" class="select-search form-control">
                             <option value=""></option>
                             @for($y=date('Y', strtotime('- 10 years')); $y<=date('Y'); $y++) <option {{ (old('year_admitted') == $y) ? 'selected' : '' }} value="{{ $y }}">{{ $y }}</option>
                                 @endfor
@@ -210,8 +210,8 @@
 
                 <div class="col-md-3">
                     <div class="form-group">
-                        <label>Admission Number:</label>
-                        <input type="text" name="adm_no" placeholder="Admission Number" class="form-control" value="{{ old('adm_no') }}">
+                        <label>رقم الإضافة:</label>
+                        <input type="text" name="adm_no" placeholder="رقم القبول" class="form-control" value="{{ old('adm_no') }}">
                     </div>
                 </div>
             </div>
